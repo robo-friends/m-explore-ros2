c3d254ee2ba1b53360d4adb9ec5431725a3face9
pierrebrd <pierrebrd7@gmail.com>
2025-07-22 10:52:05 +0200

    Edit to keep most of the code untouched

--- explore/include/explore/explore.h
+++ explore/include/explore/explore.h
@@ -109,6 +109,7 @@ private:
 
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
       marker_array_publisher_;
+  rclcpp::Logger logger_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
 
